b092182bed49ad97db2347f333d0af2ab5a6bd88
rhanflly <rhanfllyharper@gmail.com>
2024-05-11 01:14:21 -0300

    desafio-do-felipão

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("olá")
+// Definição do nome e Experiência do herói
+let NomeDoHeroi = "Kraytos";
+let xp = 10000; // Exemplo de XP
+
+// Verificação do nível do herói com base na XP
+let NivelDeCombate;
+if (xp < 1000) {
+    NivelDeCombate = "Ferro";
+} else if (xp <= 2000) {
+    NivelDeCombate = "Bronze";
+} else if (xp <= 5000) {
+    NivelDeCombate = "Prata";
+} else if (xp <= 7000) {
+    NivelDeCombate = "Ouro";
+} else if (xp <= 8000) {
+    NivelDeCombate = "Platina";
+} else if (xp <= 9000) {
+    NivelDeCombate = "Ascendente";
+} else if (xp <= 10000) {
+    NivelDeCombate = "Imortal";
+} else if (xp <= 12000) {
+    NivelDeCombate = "Radiante";
+}
+
+
+console.log(" O Herói de nome " + NomeDoHeroi + " está no nível de " + NivelDeCombate);
